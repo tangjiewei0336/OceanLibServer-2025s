65b24178ff765cfd882d5ce29a74940207d566f7
Tangxiaoxia <81951540+tangjiewei0336@users.noreply.github.com>
2025-04-19 14:40:09 +0800

    feat：新建知乎主模块
    ocean-qa-service
    目前知乎模块作为一个整体被开发。后续根据需要是否拆成更多微服务。

---
+++ ocean-qa-service/src/main/java/com/oriole/ocean/OceanQaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oriole.ocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanQaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceanQaServiceApplication.class, args);
+    }
+
+}
