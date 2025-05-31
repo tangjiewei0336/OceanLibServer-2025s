e76bf97e5ed12c78f0eb0c35147ffa94c55b924a
Tangxiaoxia <81951540+tangjiewei0336@users.noreply.github.com>
2025-06-01 01:07:23 +0800

    feat: refactor LikeController to streamline evaluateQuestion and evaluateAnswer methods for improved readability

--- ocean-qa-service/src/main/java/com/oriole/ocean/controller/LikeController.java
+++ ocean-qa-service/src/main/java/com/oriole/ocean/controller/LikeController.java
@@ -54,11 +54,7 @@ public class LikeController {
 
     @ApiOperation(value = "评价一个问题", notes = "isLike参数为评价操作的分类（true表示为点赞操作，false表示为点踩操作），isCancel为是否为取消评价操作。如isLike为false，isCancel为true，则表示取消点踩操作")
     @PostMapping(value = "/evaluateQuestion", produces = {"application/json"})
-    public MsgEntity<Map<String, Integer>> evaluateQuestion(
-            @AuthUser AuthUserEntity authUser,
-            @ApiParam(value = "问题ID", required = true) @RequestParam Integer questionId,
-            @ApiParam(value = "是否为取消操作", required = true) @RequestParam Boolean isCancel,
-            @ApiParam(value = "是否为点赞操作", required = true) @RequestParam Boolean isLike) {
+    public MsgEntity<Map<String, Integer>> evaluateQuestion(@AuthUser AuthUserEntity authUser, @ApiParam(value = "问题ID", required = true) @RequestParam Integer questionId, @ApiParam(value = "是否为取消操作", required = true) @RequestParam Boolean isCancel, @ApiParam(value = "是否为点赞操作", required = true) @RequestParam Boolean isLike) {
 
         // 获取问题信息
         QuestionEntity question = questionService.getQuestionById(questionId);
@@ -73,11 +69,7 @@ public class LikeController {
 
         // 检查是否已经点赞
         if (isLike && !isCancel) {
-            Query query = Query.query(Criteria.where("bindID").is(questionId)
-                    .and("type").is(MainType.QUESTION)
-                    .and("doUsername").is(authUser.getUsername())
-                    .and("behaviorType").is(BehaviorType.DO_LIKE));
-            UserBehaviorEntity existingLike = mongoTemplate.findOne(query, UserBehaviorEntity.class);
+            UserBehaviorEntity existingLike = userBehaviorService.findBehaviorRecord(new UserBehaviorEntity(questionId, MainType.QUESTION, authUser.getUsername(), BehaviorType.DO_LIKE));
             if (existingLike != null && !existingLike.getIsCancel()) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You have already liked this question");
             }
@@ -85,11 +77,7 @@ public class LikeController {
 
         // 检查是否已经点踩
         if (!isLike && !isCancel) {
-            Query query = Query.query(Criteria.where("bindID").is(questionId)
-                    .and("type").is(MainType.QUESTION)
-                    .and("doUsername").is(authUser.getUsername())
-                    .and("behaviorType").is(BehaviorType.DO_DISLIKE));
-            UserBehaviorEntity existingDislike = mongoTemplate.findOne(query, UserBehaviorEntity.class);
+            UserBehaviorEntity existingDislike = userBehaviorService.findBehaviorRecord(new UserBehaviorEntity(questionId, MainType.QUESTION, authUser.getUsername(), BehaviorType.DO_DISLIKE));
             if (existingDislike != null && !existingDislike.getIsCancel()) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You have already disliked this question");
             }
@@ -97,11 +85,7 @@ public class LikeController {
 
         // 检查是否可以取消点赞
         if (isLike && isCancel) {
-            Query query = Query.query(Criteria.where("bindID").is(questionId)
-                    .and("type").is(MainType.QUESTION)
-                    .and("doUsername").is(authUser.getUsername())
-                    .and("behaviorType").is(BehaviorType.DO_LIKE));
-            UserBehaviorEntity existingLike = mongoTemplate.findOne(query, UserBehaviorEntity.class);
+            UserBehaviorEntity existingLike = userBehaviorService.findBehaviorRecord(new UserBehaviorEntity(questionId, MainType.QUESTION, authUser.getUsername(), BehaviorType.DO_LIKE));
             if (existingLike == null || existingLike.getIsCancel()) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You have not liked this question yet");
             }
@@ -109,11 +93,7 @@ public class LikeController {
 
         // 检查是否可以取消点踩
         if (!isLike && isCancel) {
-            Query query = Query.query(Criteria.where("bindID").is(questionId)
-                    .and("type").is(MainType.QUESTION)
-                    .and("doUsername").is(authUser.getUsername())
-                    .and("behaviorType").is(BehaviorType.DO_DISLIKE));
-            UserBehaviorEntity existingDislike = mongoTemplate.findOne(query, UserBehaviorEntity.class);
+            UserBehaviorEntity existingDislike = userBehaviorService.findBehaviorRecord(new UserBehaviorEntity(questionId, MainType.QUESTION, authUser.getUsername(), BehaviorType.DO_DISLIKE));
             if (existingDislike == null || existingDislike.getIsCancel()) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You have not disliked this question yet");
             }
@@ -127,7 +107,7 @@ public class LikeController {
 
             // 更新问题的点赞/点踩数
             for (EvaluateType evaluate : evaluates) {
-                Query query = Query.query(Criteria.where("_id").is(questionId));
+                Query query = Query.query(Criteria.where("bind_id").is(questionId));
                 Update update = new Update();
 
                 switch (evaluate) {
@@ -172,11 +152,7 @@ public class LikeController {
 
     @ApiOperation(value = "评价一个回答", notes = "isLike参数为评价操作的分类（true表示为点赞操作，false表示为点踩操作），isCancel为是否为取消评价操作。如isLike为false，isCancel为true，则表示取消点踩操作")
     @PostMapping(value = "/evaluateAnswer", produces = {"application/json"})
-    public MsgEntity<Object> evaluateAnswer(
-            @AuthUser AuthUserEntity authUser,
-            @ApiParam(value = "回答ID", required = true) @RequestParam Integer answerId,
-            @ApiParam(value = "是否为取消操作", required = true) @RequestParam Boolean isCancel,
-            @ApiParam(value = "是否为点赞操作", required = true) @RequestParam Boolean isLike) {
+    public MsgEntity<Object> evaluateAnswer(@AuthUser AuthUserEntity authUser, @ApiParam(value = "回答ID", required = true) @RequestParam Integer answerId, @ApiParam(value = "是否为取消操作", required = true) @RequestParam Boolean isCancel, @ApiParam(value = "是否为点赞操作", required = true) @RequestParam Boolean isLike) {
 
         // 获取回答信息
         AnswerEntity answer = answerService.getAnswerById(answerId);
@@ -186,15 +162,12 @@ public class LikeController {
 
         // 检查是否是在评价自己的回答
         if (authUser.getUsername().equals(answer.getUserId())) {
-            return new MsgEntity<>("-1","ERROR","不能给自己的回答点赞或者点踩。");
+            return new MsgEntity<>("-1", "ERROR", "不能给自己的回答点赞或者点踩。");
         }
 
         // 检查是否已经点赞
         if (isLike && !isCancel) {
-            Query query = Query.query(Criteria.where("_id").is(answerId)
-                    .and("type").is(MainType.ANSWER)
-                    .and("doUsername").is(authUser.getUsername())
-                    .and("behaviorType").is(BehaviorType.DO_LIKE));
+            Query query = Query.query(Criteria.where("_id").is(answerId).and("type").is(MainType.ANSWER).and("doUsername").is(authUser.getUsername()).and("behaviorType").is(BehaviorType.DO_LIKE));
             UserBehaviorEntity existingLike = mongoTemplate.findOne(query, UserBehaviorEntity.class);
             if (existingLike != null && !existingLike.getIsCancel()) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You have already liked this answer");
@@ -203,10 +176,7 @@ public class LikeController {
 
         // 检查是否已经点踩
         if (!isLike && !isCancel) {
-            Query query = Query.query(Criteria.where("_id").is(answerId)
-                    .and("type").is(MainType.ANSWER)
-                    .and("doUsername").is(authUser.getUsername())
-                    .and("behaviorType").is(BehaviorType.DO_DISLIKE));
+            Query query = Query.query(Criteria.where("_id").is(answerId).and("type").is(MainType.ANSWER).and("doUsername").is(authUser.getUsername()).and("behaviorType").is(BehaviorType.DO_DISLIKE));
             UserBehaviorEntity existingDislike = mongoTemplate.findOne(query, UserBehaviorEntity.class);
             if (existingDislike != null && !existingDislike.getIsCancel()) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You have already disliked this answer");
@@ -226,10 +196,7 @@ public class LikeController {
             switch (evaluate) {
                 case CANCEL_LIKE:
 
-                    Query query_cancel_like = Query.query(Criteria.where("_id").is(answerId)
-                            .and("type").is(MainType.ANSWER)
-                            .and("doUsername").is(authUser.getUsername())
-                            .and("behaviorType").is(BehaviorType.DO_LIKE));
+                    Query query_cancel_like = Query.query(Criteria.where("_id").is(answerId).and("type").is(MainType.ANSWER).and("doUsername").is(authUser.getUsername()).and("behaviorType").is(BehaviorType.DO_LIKE));
                     UserBehaviorEntity existingLike = mongoTemplate.findOne(query_cancel_like, UserBehaviorEntity.class);
                     if (!(existingLike == null || existingLike.getIsCancel())) {
                         userBehaviorService.deleteBehaviorRecord(existingLike);
@@ -240,10 +207,7 @@ public class LikeController {
                     update.inc("like_count", 1);
                     break;
                 case CANCEL_DISLIKE:
-                    Query cancel_dislike_query = Query.query(Criteria.where("_id").is(answerId)
-                            .and("type").is(MainType.ANSWER)
-                            .and("doUsername").is(authUser.getUsername())
-                            .and("behaviorType").is(BehaviorType.DO_DISLIKE));
+                    Query cancel_dislike_query = Query.query(Criteria.where("_id").is(answerId).and("type").is(MainType.ANSWER).and("doUsername").is(authUser.getUsername()).and("behaviorType").is(BehaviorType.DO_DISLIKE));
                     UserBehaviorEntity existingDislike = mongoTemplate.findOne(cancel_dislike_query, UserBehaviorEntity.class);
                     if (!(existingDislike == null || existingDislike.getIsCancel())) {
                         update.inc("dislike_count", -1);
---
+++ ocean-qa-service/src/test/java/com/oriole/ocean/integration/LikeControllerIntegrationTest.java
@@ -0,0 +1,263 @@
+package com.oriole.ocean.integration;
+
+import com.oriole.ocean.common.auth.AuthUser;
+import com.oriole.ocean.common.enumerate.EvaluateType;
+import com.oriole.ocean.common.enumerate.MainType;
+import com.oriole.ocean.common.po.mongo.AnswerEntity;
+import com.oriole.ocean.common.po.mongo.QuestionEntity;
+import com.oriole.ocean.common.po.mongo.UserBehaviorEntity;
+import com.oriole.ocean.common.service.NotifyService;
+import com.oriole.ocean.common.service.UserBehaviorService;
+import com.oriole.ocean.common.vo.AuthUserEntity;
+import com.oriole.ocean.common.vo.MsgEntity;
+import com.oriole.ocean.controller.LikeController;
+import com.oriole.ocean.service.AnswerService;
+import com.oriole.ocean.service.QuestionService;
+import com.oriole.ocean.service.SequenceGeneratorService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class LikeControllerIntegrationTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Autowired
+    private AnswerService answerService;
+
+    @Autowired
+    private LikeController likeController;
+
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
+    @MockBean
+    private UserBehaviorService userBehaviorService;
+
+    @MockBean
+    private NotifyService notifyService;
+
+    private Integer testQuestionId;
+    private Integer testAnswerId;
+    private AuthUserEntity testUser;
+    private AuthUserEntity otherUser;
+
+    @BeforeEach
+    void setUp() {
+        // 清理测试数据
+        mongoTemplate.remove(new Query(), QuestionEntity.class);
+        mongoTemplate.remove(new Query(), AnswerEntity.class);
+        mongoTemplate.remove(new Query(), UserBehaviorEntity.class);
+
+        // 创建测试用户
+        String testUserId = "testUser";
+        testUser = new AuthUserEntity(testUserId, "USER");
+        String otherUserId = "otherUser";
+        otherUser = new AuthUserEntity(otherUserId, "USER");
+
+        // 创建测试问题
+        QuestionEntity question = new QuestionEntity();
+        question.setBindId(sequenceGeneratorService.getNextSequence("question"));
+        question.setUserId(testUserId);
+        question.setTitle("Test Question");
+        question.setContent("Test Content");
+        question.setIsPosted(true);
+        question.setIsHidden(false);
+        question.setLikeCount(0);
+        question.setDislikeCount(0);
+        mongoTemplate.save(question);
+        testQuestionId = question.getBindId();
+
+        // 创建测试回答
+        AnswerEntity answer = new AnswerEntity();
+        answer.setId(sequenceGeneratorService.getNextSequence("answer"));
+        answer.setQuestionId(testQuestionId);
+        answer.setUserId(otherUserId);
+        answer.setContent("Test Answer");
+        answer.setLikeCount(0);
+        answer.setDislikeCount(0);
+        mongoTemplate.save(answer);
+        testAnswerId = answer.getId();
+    }
+
+    @Test
+    void testEvaluateQuestion_Like() {
+        // 准备测试数据
+        when(userBehaviorService.checkAndGetUserEvaluateBehavior(any(), eq(false), eq(true)))
+                .thenReturn(Arrays.asList(EvaluateType.LIKE));
+
+        // 执行点赞操作
+        MsgEntity<Map<String, Integer>> result = likeController.evaluateQuestion(
+                otherUser, testQuestionId, false, true);
+
+        // 验证结果
+        assertEquals("SUCCESS", result.getState());
+        assertEquals("1", result.getCode());
+        Map<String, Integer> data = result.getMsg();
+        assertEquals(1, data.get("like_count"));
+        assertEquals(0, data.get("dislike_count"));
+        assertEquals(1, data.get("net_count"));
+
+        // 验证问题数据更新
+        QuestionEntity updatedQuestion = questionService.getQuestionById(testQuestionId);
+        assertEquals(1, updatedQuestion.getLikeCount());
+        assertEquals(0, updatedQuestion.getDislikeCount());
+
+        // 验证通知发送
+        verify(notifyService, times(1)).addNotify(any());
+    }
+
+    @Test
+    void testEvaluateQuestion_Dislike() {
+        // 准备测试数据
+        when(userBehaviorService.checkAndGetUserEvaluateBehavior(any(), eq(false), eq(false)))
+                .thenReturn(Arrays.asList(EvaluateType.DISLIKE));
+
+        // 执行点踩操作
+        MsgEntity<Map<String, Integer>> result = likeController.evaluateQuestion(
+                otherUser, testQuestionId, false, false);
+
+        // 验证结果
+        assertEquals("SUCCESS", result.getState());
+        assertEquals("1", result.getCode());
+        Map<String, Integer> data = result.getMsg();
+        assertEquals(0, data.get("like_count"));
+        assertEquals(1, data.get("dislike_count"));
+        assertEquals(-1, data.get("net_count"));
+
+        // 验证问题数据更新
+        QuestionEntity updatedQuestion = questionService.getQuestionById(testQuestionId);
+        assertEquals(0, updatedQuestion.getLikeCount());
+        assertEquals(1, updatedQuestion.getDislikeCount());
+
+        // 验证没有发送通知
+        verify(notifyService, never()).addNotify(any());
+    }
+
+    @Test
+    void testEvaluateQuestion_CancelLike() {
+        // 准备测试数据
+        when(userBehaviorService.checkAndGetUserEvaluateBehavior(any(), eq(true), eq(true)))
+                .thenReturn(Arrays.asList(EvaluateType.CANCEL_LIKE));
+
+        // 先点赞
+        likeController.evaluateQuestion(otherUser, testQuestionId, false, true);
+
+        // 取消点赞
+        MsgEntity<Map<String, Integer>> result = likeController.evaluateQuestion(
+                otherUser, testQuestionId, true, true);
+
+        // 验证结果
+        assertEquals("SUCCESS", result.getState());
+        assertEquals("1", result.getCode());
+        Map<String, Integer> data = result.getMsg();
+        assertEquals(0, data.get("like_count"));
+        assertEquals(0, data.get("dislike_count"));
+        assertEquals(0, data.get("net_count"));
+
+        // 验证问题数据更新
+        QuestionEntity updatedQuestion = questionService.getQuestionById(testQuestionId);
+        assertEquals(0, updatedQuestion.getLikeCount());
+        assertEquals(0, updatedQuestion.getDislikeCount());
+    }
+
+    @Test
+    void testEvaluateQuestion_SelfEvaluation() {
+        // 尝试对自己的问题进行评价
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () ->
+                likeController.evaluateQuestion(testUser, testQuestionId, false, true));
+
+        assertEquals(HttpStatus.FORBIDDEN, exception.getStatus());
+        assertEquals("Cannot evaluate your own question", exception.getReason());
+    }
+
+    @Test
+    void testEvaluateAnswer_Like() {
+        // 准备测试数据
+        when(userBehaviorService.checkAndGetUserEvaluateBehavior(any(), eq(false), eq(true)))
+                .thenReturn(Arrays.asList(EvaluateType.LIKE));
+
+        // 执行点赞操作
+        MsgEntity<Object> result = likeController.evaluateAnswer(
+                testUser, testAnswerId, false, true);
+
+        // 验证结果
+        assertEquals("SUCCESS", result.getState());
+        assertEquals("1", result.getCode());
+        Map<String, Integer> data = (Map<String, Integer>) result.getMsg();
+        assertEquals(1, data.get("like_count"));
+        assertEquals(0, data.get("dislike_count"));
+        assertEquals(1, data.get("net_count"));
+
+        // 验证回答数据更新
+        AnswerEntity updatedAnswer = answerService.getAnswerById(testAnswerId);
+        assertEquals(1, updatedAnswer.getLikeCount());
+        assertEquals(0, updatedAnswer.getDislikeCount());
+
+        // 验证通知发送
+        verify(notifyService, times(1)).addNotify(any());
+    }
+
+    @Test
+    void testEvaluateAnswer_Dislike() {
+        // 准备测试数据
+        when(userBehaviorService.checkAndGetUserEvaluateBehavior(any(), eq(false), eq(false)))
+                .thenReturn(Arrays.asList(EvaluateType.DISLIKE));
+
+        // 执行点踩操作
+        MsgEntity<Object> result = likeController.evaluateAnswer(
+                testUser, testAnswerId, false, false);
+
+        // 验证结果
+        assertEquals("SUCCESS", result.getState());
+        assertEquals("1", result.getCode());
+        Map<String, Integer> data = (Map<String, Integer>) result.getMsg();
+        assertEquals(0, data.get("like_count"));
+        assertEquals(1, data.get("dislike_count"));
+        assertEquals(-1, data.get("net_count"));
+
+        // 验证回答数据更新
+        AnswerEntity updatedAnswer = answerService.getAnswerById(testAnswerId);
+        assertEquals(0, updatedAnswer.getLikeCount());
+        assertEquals(1, updatedAnswer.getDislikeCount());
+
+        // 验证没有发送通知
+        verify(notifyService, never()).addNotify(any());
+    }
+
+    @Test
+    void testEvaluateAnswer_SelfEvaluation() {
+        // 尝试对自己的回答进行评价
+        MsgEntity<Object> result = likeController.evaluateAnswer(
+                otherUser, testAnswerId, false, true);
+
+        // 验证结果
+        assertEquals("-1", result.getState());
+        assertEquals("ERROR", result.getCode());
+        assertEquals("不能给自己的回答点赞或者点踩。", result.getMsg());
+    }
+} 
